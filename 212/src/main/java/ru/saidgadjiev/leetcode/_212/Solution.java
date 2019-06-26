780c68def86d4dd36ee062e056d4cc8e43c9b07c
GadzhievSA <s.gadzhiev@id-mt.ru>
2019-06-26 16:15:56 +0300

    212 добавил условие задачи

--- 212/src/main/java/ru/saidgadjiev/leetcode/_212/Solution.java
+++ 212/src/main/java/ru/saidgadjiev/leetcode/_212/Solution.java
@@ -3,6 +3,32 @@ package ru.saidgadjiev.leetcode._212;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a 2D board and a list of words from the dictionary, find all words in the board.
+ *
+ * Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+ *
+ *
+ *
+ * Example:
+ *
+ * Input:
+ * board = [
+ *   ['o','a','a','n'],
+ *   ['e','t','a','e'],
+ *   ['i','h','k','r'],
+ *   ['i','f','l','v']
+ * ]
+ * words = ["oath","pea","eat","rain"]
+ *
+ * Output: ["eat","oath"]
+ *
+ *
+ * Note:
+ *
+ * All inputs are consist of lowercase letters a-z.
+ * The values of words are distinct.
+ */
 public class Solution {
 
     public List<String> findWords(char[][] board, String[] words) {
