2ca4ec680eeb2e1891a7494f40c53135fd6ee190
GadzhievSA <s.gadzhiev@id-mt.ru>
2019-06-28 09:02:46 +0300

    70

--- easy/climbing-stairs/src/main/java/ru/saidgadjiev/leetcode/easy/_70/Solution.java
+++ easy/climbing-stairs/src/main/java/ru/saidgadjiev/leetcode/easy/_70/Solution.java
@@ -26,6 +26,18 @@ package ru.saidgadjiev.leetcode.easy._70;
 public class Solution {
 
     public int climbStairs(int n) {
-        return 0;
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n];
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n - 1];
     }
 }
