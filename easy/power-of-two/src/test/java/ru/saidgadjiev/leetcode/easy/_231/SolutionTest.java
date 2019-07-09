f0a29e28837da6541761b079897d63be8335eb8c
GadzhievSA <s.gadzhiev@id-mt.ru>
2019-07-09 15:12:03 +0300

    190

--- easy/power-of-two/src/test/java/ru/saidgadjiev/leetcode/easy/_231/SolutionTest.java
+++ easy/power-of-two/src/test/java/ru/saidgadjiev/leetcode/easy/_231/SolutionTest.java
@@ -3,7 +3,6 @@ package ru.saidgadjiev.leetcode.easy._231;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
---
+++ easy/reverse-bits/src/test/java/ru/saidgadjiev/leetcode/easy/_190/SolutionTest.java
@@ -0,0 +1,16 @@
+package ru.saidgadjiev.leetcode.easy._190;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void reverseBits() {
+        int result = new Solution().reverseBits(-3);
+
+        Assertions.assertEquals(964176192, result);
+    }
+}
