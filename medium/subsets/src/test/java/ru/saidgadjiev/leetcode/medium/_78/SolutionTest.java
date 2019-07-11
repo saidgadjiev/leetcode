0b0303e5797dc3a5dd54db95dcbf5e435f73e18c
GadzhievSA <s.gadzhiev@id-mt.ru>
2019-07-11 17:11:10 +0300

    subsets

---
+++ medium/subsets/src/test/java/ru/saidgadjiev/leetcode/medium/_78/SolutionTest.java
@@ -0,0 +1,13 @@
+package ru.saidgadjiev.leetcode.medium._78;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void subsets() {
+        new Solution().subsets(new int[] { 1, 2, 3 });
+    }
+}
