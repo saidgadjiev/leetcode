25c67428c3258e2adb550fcef3725ed21693fe17
GadzhievSA <said.gadzhiev@id-mt.ru>
2019-07-22 19:22:11 +0300

    784

--- easy/letter-case-permutation/src/main/java/ru/saidgadjiev/leetcode/easy/_784/Solution.java
+++ easy/letter-case-permutation/src/main/java/ru/saidgadjiev/leetcode/easy/_784/Solution.java
@@ -3,9 +3,57 @@ package ru.saidgadjiev.leetcode.easy._784;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
+ * <p>
+ * Examples:
+ * Input: S = "a1b2"
+ * Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+ * <p>
+ * Input: S = "3z4"
+ * Output: ["3z4", "3Z4"]
+ * <p>
+ * Input: S = "12345"
+ * Output: ["12345"]
+ * Note:
+ * <p>
+ * S will be a string with length between 1 and 12.
+ * S will consist only of letters or digits.
+ */
 public class Solution {
 
     public List<String> letterCasePermutation(String S) {
+        permute(S, 0, S.length() -1);
         return new ArrayList<>();
     }
+
+    private void permute(String str, int l, int r) {
+        if (l == r)
+            System.out.println(str);
+        else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permute(str, l + 1, r);
+                str = swap(str, l, i);
+            }
+        }
+    }
+
+    /**
+     * Swap Characters at position
+     *
+     * @param a string value
+     * @param i position 1
+     * @param j position 2
+     * @return swapped string
+     */
+    public String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
+    }
+
 }
