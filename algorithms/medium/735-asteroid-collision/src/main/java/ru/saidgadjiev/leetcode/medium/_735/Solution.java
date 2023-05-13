ac3d31a809d4047c2122253b00e8e728eef85035
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-05-13 22:37:50 +0300

    735. Asteroid Collision

---
+++ algorithms/medium/735-asteroid-collision/src/main/java/ru/saidgadjiev/leetcode/medium/_735/Solution.java
@@ -0,0 +1,29 @@
+package ru.saidgadjiev.leetcode.medium._735;
+
+import java.util.Stack;
+
+public class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack();
+        for (int ast: asteroids) {
+            collision: {
+                while (!stack.isEmpty() && ast < 0 && 0 < stack.peek()) {
+                    if (stack.peek() < -ast) {
+                        stack.pop();
+                        continue;
+                    } else if (stack.peek() == -ast) {
+                        stack.pop();
+                    }
+                    break collision;
+                }
+                stack.push(ast);
+            }
+        }
+
+        int[] ans = new int[stack.size()];
+        for (int t = ans.length - 1; t >= 0; --t) {
+            ans[t] = stack.pop();
+        }
+        return ans;
+    }
+}
