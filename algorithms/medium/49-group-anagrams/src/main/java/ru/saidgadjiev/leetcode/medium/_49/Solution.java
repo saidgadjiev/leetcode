73a29b5d9a8105ebd22d0011178e4329bc223eb9
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-05-15 19:12:04 +0300

    49. Group Anagrams

--- algorithms/medium/49-group-anagrams/src/main/java/ru/saidgadjiev/leetcode/medium/_49/Solution.java
+++ algorithms/medium/49-group-anagrams/src/main/java/ru/saidgadjiev/leetcode/medium/_49/Solution.java
@@ -9,50 +9,15 @@ import java.util.*;
  */
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> candidateGroups = new HashMap<>();
-        Map<String, Map<Character, Integer>> keyWords = new HashMap<>();
-
-        for (String str : strs) {
-            boolean grouped = false;
-            for (String s : candidateGroups.keySet()) {
-                if (isAnagram(str, keyWords.get(s))) {
-                    candidateGroups.get(s).add(str);
-                    grouped = true;
-                }
-            }
-            if (!grouped) {
-                candidateGroups.put(str, new ArrayList<>());
-                candidateGroups.get(str).add(str);
-                Map<Character, Integer> frequency = frequency(str);
-                keyWords.put(str, frequency);
-            }
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
         }
-
-        return new ArrayList<>(candidateGroups.values());
-    }
-
-    private Map<Character, Integer> frequency(String word) {
-        Map<Character, Integer> frequency = new HashMap<>();
-        for (char c : word.toCharArray()) {
-            frequency.put(c, frequency.getOrDefault(c, 0) + 1);
-        }
-
-        return frequency;
-    }
-
-    private boolean isAnagram(String candidateWord, Map<Character, Integer> keywords) {
-        Map<Character, Integer> frequency = frequency(candidateWord);
-
-        if (keywords.size() != frequency.size()) {
-            return false;
-        }
-
-        for (Character character : frequency.keySet()) {
-            if (!Objects.equals(frequency.get(character), keywords.get(character))) {
-                return false;
-            }
-        }
-
-        return true;
+        return new ArrayList<>(ans.values());
     }
 }
