fb9dab8b62bcd02abc93624ee59e6b5991e3721a
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-06-15 09:01:50 +0300

    239. Sliding Window Maximum

--- algorithms/hard/239-sliding-window-maximum/src/main/java/ru/saidgadjiev/leetcode/medium/_239/Solution.java
+++ algorithms/hard/239-sliding-window-maximum/src/main/java/ru/saidgadjiev/leetcode/medium/_239/Solution.java
@@ -1,9 +1,6 @@
 package ru.saidgadjiev.leetcode.medium._239;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * https://leetcode.com/problems/sliding-window-maximum
@@ -13,28 +10,49 @@ import java.util.PriorityQueue;
  * Window position                Max
  * ---------------               -----
  * [1  3  -1] -3  5  3  6  7       3
- *  1 [3  -1  -3] 5  3  6  7       3
- *  1  3 [-1  -3  5] 3  6  7       5
- *  1  3  -1 [-3  5  3] 6  7       5
- *  1  3  -1  -3 [5  3  6] 7       6
- *  1  3  -1  -3  5 [3  6  7]      7
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
  */
 public class Solution {
+    ArrayDeque<Integer> deq = new ArrayDeque<Integer>();
+    int[] nums;
+
+    public void clean_deque(int i, int k) {
+        // remove indexes of elements not from sliding window
+        if (!deq.isEmpty() && deq.getFirst() == i - k)
+            deq.removeFirst();
+
+        // remove from deq indexes of all elements
+        // which are smaller than current element nums[i]
+        while (!deq.isEmpty() && nums[i] > nums[deq.getLast()]) deq.removeLast();
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = nums.length;
+        if (n * k == 0) return new int[0];
+        if (k == 1) return nums;
 
+        // init deque and output
+        this.nums = nums;
+        int max_idx = 0;
         for (int i = 0; i < k; i++) {
-            priorityQueue.offer(nums[i]);
+            clean_deque(i, k);
+            deq.addLast(i);
+            // compute max in nums[:k]
+            if (nums[i] > nums[max_idx]) max_idx = i;
         }
+        int[] output = new int[n - k + 1];
+        output[0] = nums[max_idx];
 
-        List<Integer> result = new ArrayList<>();
-        result.add(priorityQueue.peek());
-        for (int i = k; i < nums.length; i++) {
-            priorityQueue.remove(nums[i - k]);
-            priorityQueue.offer(nums[i]);
-            result.add(priorityQueue.peek());
+        // build output
+        for (int i = k; i < n; i++) {
+            clean_deque(i, k);
+            deq.addLast(i);
+            output[i - k + 1] = nums[deq.getFirst()];
         }
-
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return output;
     }
 }
