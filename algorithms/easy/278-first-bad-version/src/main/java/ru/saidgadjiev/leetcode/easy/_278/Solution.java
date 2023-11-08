dbca4eef6db8cf8d403503bb928443b52e52254e
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-11-08 08:51:26 +0300

    278. First Bad Version

--- algorithms/easy/278-first-bad-version/src/main/java/ru/saidgadjiev/leetcode/easy/_278/Solution.java
+++ algorithms/easy/278-first-bad-version/src/main/java/ru/saidgadjiev/leetcode/easy/_278/Solution.java
@@ -2,26 +2,17 @@ package ru.saidgadjiev.leetcode.easy._278;
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        long left = 0;
-        long right = n;
-
-        long lastBadVersion = -1;
-        while (left <= right) {
-            long mid = (left + right) / 2;
-            boolean midVersionStatus = isBadVersion((int) mid);
-            if (midVersionStatus) {
-                lastBadVersion = mid;
-                right = mid - 1;
+        int i = 1, j = n;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            boolean isBad = isBadVersion(mid);
+            if (isBad) {
+                j = mid - 1;
             } else {
-                left = mid + 1;
+                i = mid + 1;
             }
         }
 
-        return (int) lastBadVersion;
-    }
-
-    public static void main(String[] args) {
-        int i = new Solution().firstBadVersion(16);
-        System.out.println(i);
+        return i;
     }
 }
