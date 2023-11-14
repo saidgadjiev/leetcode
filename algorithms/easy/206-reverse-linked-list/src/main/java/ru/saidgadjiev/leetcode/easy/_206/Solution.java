2baff14244e073edd230de39d12dc6d80111de25
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-11-14 13:37:48 +0300

    206. Reverse Linked List

--- algorithms/easy/206-reverse-linked-list/src/main/java/ru/saidgadjiev/leetcode/easy/_206/Solution.java
+++ algorithms/easy/206-reverse-linked-list/src/main/java/ru/saidgadjiev/leetcode/easy/_206/Solution.java
@@ -1,57 +1,31 @@
 package ru.saidgadjiev.leetcode.easy._206;
 
+import ru.saidgadjiev.leetcode.common.ListNode;
+
 /**
  * https://leetcode.com/problems/reverse-linked-list
+ *
+ * 1 2 3 4 5
+ *
+ * 1->null
+ * 2->1->null
+ * 3->2->1->null
+ * 4->3->2->1->null
+ * 5->4->3->2->1->null
  **/
 public class Solution {
 
     public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
-            return head;
-        }
-        ListNode prev = head;
-        ListNode result = head.next;
-        head = head.next.next;
-        result.next = prev;
-        prev.next = null;
-        while (head != null) {
-            prev = result;
-            result = head;
-            head = head.next;
-            result.next = prev;
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTmpNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTmpNode;
         }
 
-        return result;
+        return prev;
     }
 
-    public static void main(String[] args) {
-        ListNode list1 = new ListNode();
-        list1.val = 1;
-
-        ListNode listNode = new Solution().reverseList(list1);
-        while (listNode != null) {
-            System.out.println(listNode.val);
-            listNode = listNode.next;
-        }
-    }
-
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 }
