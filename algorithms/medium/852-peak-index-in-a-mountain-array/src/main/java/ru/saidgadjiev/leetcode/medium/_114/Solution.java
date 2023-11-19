c79745178f53f2371ef000803d414c92508e201e
GadzhievSA <s.gadzhiev@id-mt.ru>
2023-11-19 09:06:29 +0300

    852. Peak Index in a Mountain Array

---
+++ algorithms/medium/852-peak-index-in-a-mountain-array/src/main/java/ru/saidgadjiev/leetcode/medium/_114/Solution.java
@@ -0,0 +1,20 @@
+package ru.saidgadjiev.leetcode.medium._114;
+
+public class Solution {
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0; int r = arr.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return arr[l];
+    }
+}
