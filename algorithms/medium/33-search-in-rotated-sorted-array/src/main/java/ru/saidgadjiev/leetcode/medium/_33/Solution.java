0c7ec574bfc461a3f2b2b62be212aae3868a6faa
GadzhievSA <s.gadzhiev@id-mt.ru>
2024-07-12 17:43:40 +0300

    33

--- algorithms/medium/33-search-in-rotated-sorted-array/src/main/java/ru/saidgadjiev/leetcode/medium/_33/Solution.java
+++ algorithms/medium/33-search-in-rotated-sorted-array/src/main/java/ru/saidgadjiev/leetcode/medium/_33/Solution.java
@@ -7,28 +7,12 @@ package ru.saidgadjiev.leetcode.medium._33;
 public class Solution {
 
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[mid] >= nums[start]) {
-                if (nums[mid] > target && target >= nums[start]) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
-            }
-            if (nums[mid] <= nums[end]) {
-                if (target <= nums[end] && target > nums[mid]) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
-        }
+        int a, b , c;
+        b = 10;
+        a = b = c = 20;
+
+
+        System.out.println(a);
 
         return -1;
     }
