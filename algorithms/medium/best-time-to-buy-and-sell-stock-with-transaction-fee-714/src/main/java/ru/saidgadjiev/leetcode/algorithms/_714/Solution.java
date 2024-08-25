f7eb18ace8d8d42ce3334fe04669fe2cf082dece
Gadzhiev Said <s.gadzhiev@id-mt.ru>
2024-08-25 17:05:06 +0300

    714. Best Time to Buy and Sell Stock with Transaction Fee

--- algorithms/medium/best-time-to-buy-and-sell-stock-with-transaction-fee-714/src/main/java/ru/saidgadjiev/leetcode/algorithms/_714/Solution.java
+++ algorithms/medium/best-time-to-buy-and-sell-stock-with-transaction-fee-714/src/main/java/ru/saidgadjiev/leetcode/algorithms/_714/Solution.java
@@ -3,6 +3,16 @@ package ru.saidgadjiev.leetcode.algorithms._714;
 public class Solution {
 
     public int maxProfit(int[] prices, int fee) {
-        return 0;
+        int[][] dp = new int[prices.length][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0] - fee;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i] - fee);
+        }
+
+        return Math.max(dp[prices.length - 1][0], dp[prices.length - 1][1]);
     }
 }
